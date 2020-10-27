884a2271f19f7bb7fbf2fa878ebfbbc150660937
Jill Hansalia <jillhansalia@gmail.com.com>
2020-10-27 22:56:31 +0530

    Fix issue #4

--- Sort an array of 0s, 1s and 2s.java
+++ Sort an array of 0s, 1s and 2s.java
@@ -6,7 +6,31 @@ class countzot {
     // have values in {0, 1, 2} 
     static void sort012(int a[], int arr_size) 
     { 
-        
+        int lo = 0; 
+        int hi = arr_size - 1; 
+        int mid = 0, temp = 0; 
+        while (mid <= hi) { 
+            switch (a[mid]) { 
+            case 0: { 
+                temp = a[lo]; 
+                a[lo] = a[mid]; 
+                a[mid] = temp; 
+                lo++; 
+                mid++; 
+                break; 
+            } 
+            case 1: 
+                mid++; 
+                break; 
+            case 2: { 
+                temp = a[mid]; 
+                a[mid] = a[hi]; 
+                a[hi] = temp; 
+                hi--; 
+                break; 
+            } 
+            } 
+        } 
     } 
   
     /* Utility function to print array arr[] */
